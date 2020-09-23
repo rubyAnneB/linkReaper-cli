984f716544caef39e91f5032764d4f70552480a4
Ruby <bautisruby643@gmail.com>
2020-09-23 19:59:41 -0400

    added support for different http codes

--- linkReaper/linkReaper.py
+++ linkReaper/linkReaper.py
@@ -1,7 +1,7 @@
-from urllib import request
-from urllib.parse import urlparse
+from urllib3 import request
 import click
 import re
+import urllib3
 
 
 #
@@ -20,19 +20,39 @@ def main():
 def readfile(filepath):
     """Read from a local file"""
 
+    # TODO: get support for HTTP to HTTPS
+
     with open(filepath, 'r') as file:
-        regex = 'http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
         urls = re.findall(r'https?:[a-zA-Z0-9_.+-/#~]+', file.read())
-        urls = list(dict.fromkeys(urls))
+        r = []
+        [r.append(x) for x in urls if x not in r]
+        retrieve_codes(r)
 
 
-        for link in urls:
-            resp = request.urlopen(link)
-            if resp.code == 200:
-                click.echo(click.style("GOOD " + str(resp.code) + " " + link, fg='green'))
+def retrieve_codes(links):
+    # goes through the list of links and retrieves the htpp responses and displays them
+    for link in links:
 
-            if resp.code == 403:
-                click.echo(click.style("BAD " + str(resp.code) + " " + link, fg='red'))
+        try:
+            pool = urllib3.PoolManager()
+            response = pool.request('HEAD', link)
+            if 300 > response.status <= 200:
+                # successful responses
+                click.echo(click.style("GOOD      - Successful  : " + str(response.status) + " " + link, fg='green'))
+            elif 400 > response.status <= 300:
+                # redirection message
+                click.echo(click.style("BAD       - Redirect    : " + str(response.status) + " " + link, fg='red'))
+            elif 500 > response.status <= 400:
+                # client error responses
+                click.echo(click.style("BAD       - Client Error: " + str(response.status) + " " + link, fg='red'))
+            elif 600 > response.status <= 500:
+                # server error response
+                click.echo(click.style("BAD       - Server Error: " + str(response.status) + " " + link, fg='red'))
+            else:
+                click.echo(click.style("Unknown " + str(response.status) + " " + link, fg='red'))
+
+        except Exception:
+            click.echo(click.style("Irregular -  Code       :" + str(response.status) + " " + link, fg='yellow'))
 
 
 @main.command()
