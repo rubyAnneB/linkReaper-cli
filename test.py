61142b750773ccd79fba8f652a922cbaad89eebc
Ruby <bautisruby643@gmail.com>
2020-11-27 16:13:37 -0500

    added test.py to begin writing tests

---
+++ test.py
@@ -0,0 +1,15 @@
+"""This is the file that contains all of the tests for LinkReaper"""
+import unittest
+# import linkReaper
+
+
+class TestLinkReaper(unittest.TestCase):
+    """Test Class for LinkReaper"""
+    def testme(self):
+        """Test for testing"""
+        result = sum([1, 2, 3])
+        self.assertEqual(result, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
