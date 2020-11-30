246290603207342f2695c300f96b1bdc2e9a3dbd
Ruby <bautisruby643@gmail.com>
2020-11-29 22:30:55 -0500

    updated version number

--- linkReaper/setup.py
+++ linkReaper/setup.py
@@ -1,8 +1,9 @@
+"""Setup file detailing dependencies"""
 from setuptools import setup
 
 setup(
     name="linkreaper",
-    version="0.1",
+    version="1.3",
     install_requires=["Click", "Colorama", "urllib3", "black", "pylint"],
     py_modules=["linkReaper"],
     entry_points={
