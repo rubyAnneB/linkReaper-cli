4b6e93bd854ab29266766cab9614f25838d1894e
Ruby <bautisruby643@gmail.com>
2020-12-04 02:56:07 -0500

    added some details to setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""Setup file detailing dependencies"""
+from setuptools import setup
+
+setup(
+    name="linkreaper",
+    author = "rubyAnneB",
+    author_email="bautisruby643@gmail.com",
+    version="0.0.3",
+    install_requires=["Click", "Colorama", "urllib3"],
+    py_modules=["linkreaper"],
+    entry_points={
+        "console_scripts": [
+            "linkreaper= linkreaper:main",
+        ]
+    },
+    description="checks for deadlinks in an html file or from a website",
+    package_dir={"": "src"},
+    url="https://github.com/rubyAnneB/linkReaper-cli",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
